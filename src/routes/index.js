a9f7ec572f7795951b7fd6d5796adfbfe6bb90cf
Jackson Barr <jacksonbarr2021@gmail.com>
2025-11-26 17:23:15 -0500

    Add event routes to routes wrapper

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,12 +1,14 @@
 const express = require("express");
 const authRoutes = require("./auth.routes");
 const bandRoutes = require("./band.routes");
+const eventRoutes = require("./event.routes");
 const experimentRoutes = require("./experiment.routes");
 
 const router = express.Router();
 
 router.use("/auth", authRoutes);
 router.use("/bands", bandRoutes);
+router.use("/events", eventRoutes);
 router.use('/experiments', experimentRoutes);
 
 module.exports = router;
